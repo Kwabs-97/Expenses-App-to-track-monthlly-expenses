1126e8b9d7204fde8d6ecf41d4d637be66020988
Kwabs-97 <107767920+Kwabs-97@users.noreply.github.com>
2023-04-26 10:16:42 +0000

    Add files via upload

---
+++ build/precache-manifest.ed46986f04e9a9bc75cfa1de4bb92d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9a991096c265dc44c9c",
+    "url": "/static/css/main.c207f8b8.chunk.css"
+  },
+  {
+    "revision": "c9a991096c265dc44c9c",
+    "url": "/static/js/main.c9a99109.chunk.js"
+  },
+  {
+    "revision": "a750be8a448f75d38824",
+    "url": "/static/js/1.a750be8a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4048a4cfae1da5fed82af8815141ccc2",
+    "url": "/index.html"
+  }
+];
